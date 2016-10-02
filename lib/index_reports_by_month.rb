be2b169e185921d47ce7377cb03d7495becb5135
Jim Kirkbride <james.kirkbride@gmail.com>
2016-10-02 08:29:49 -0400

    Populating full monthly report link.

--- lib/index_reports_by_month.rb
+++ lib/index_reports_by_month.rb
@@ -1,14 +1,23 @@
-# require 'open-uri'
+require 'open-uri'
+require 'nokogiri'
+require_relative 'monthly_report'
 
 class IndexReportsByMonth
-  #
-  #
-  # def initialize(url)
-  #   # @url = url
-  # end
-  #
-  # def parse
-  #   # puts "Parsing url: #{@url}"
-  #   # @doc = Nokogiri::HTML(open(url))
-  # end
+
+
+  def initialize(url)
+    @url = url
+  end
+
+  def parse
+    puts "Parsing url: #{@url}"
+    doc = Nokogiri::HTML(open(@url))
+    monthly_report_links = doc.xpath("//table/tbody/tr/td/font/a/@href")
+    puts monthly_report_links
+    monthly_reports = monthly_report_links.map {|link|
+      MonthlyReport.new("http://nuforc.org/webreports/#{link}")
+    }
+
+    monthly_reports
+  end
 end
--- spec/index_reports_by_month_spec.rb
+++ spec/index_reports_by_month_spec.rb
@@ -1,6 +1,35 @@
 describe IndexReportsByMonth do
 
+  before(:each) do
+    path = File.expand_path File.join(File.dirname(__FILE__), 'reports_by_month.html')
+    @index_reports_html = File::open(path)
+  end
+
+  after(:each) do
+    @index_reports_html.close
+  end
+
   it 'should return a list of monthly reports' do
-    
+    expected_url = 'http://nuforc.org/webreports/ndxevent.html'
+    index_reports = IndexReportsByMonth.new(expected_url)
+
+    expect(index_reports).to receive(:open).with(expected_url).and_return(@index_reports_html)
+
+    monthly_reports = index_reports.parse
+    puts monthly_reports.select {|report| report != nil}.empty?
+
+    expect(monthly_reports.length).to eq(888)
+  end
+
+  it 'should populate the month reports with a full url' do
+    expected_url = 'http://nuforc.org/webreports/ndxevent.html'
+    index_reports = IndexReportsByMonth.new(expected_url)
+
+    expect(index_reports).to receive(:open).with(expected_url).and_return(@index_reports_html)
+
+    monthly_reports = index_reports.parse
+    puts monthly_reports.select {|report| report != nil}.empty?
+
+    expect(monthly_reports[0].url).to eq('http://nuforc.org/webreports/ndxe201609.html')
   end
 end
