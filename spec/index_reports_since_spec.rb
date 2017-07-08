f144d51812612abe4e1fe77981355054cf09f1e3
Jim Kirkbride <james.kirkbride@gmail.com>
2017-07-08 15:08:43 -0400

    Adding coverage for returning an empty list of reports given a date after the last posted date

--- spec/index_reports_since_spec.rb
+++ spec/index_reports_since_spec.rb
@@ -2,7 +2,6 @@ describe IndexReportsSince do
 
   ARBITRARY_URL = 'http://nuforc.org/webreports/ndxpost.html'
 
-
   before(:each) do
     path = File.expand_path File.join(File.dirname(__FILE__), 'reports_since.html')
     @index_reports_html = File::open(path)
@@ -17,6 +16,7 @@ describe IndexReportsSince do
       exact_date = '07/07/2017'
       index_reports = IndexReportsSince.new(ARBITRARY_URL, exact_date)
       expect(index_reports).to receive(:open).with(ARBITRARY_URL).and_return(@index_reports_html)
+
       @monthly_reports = index_reports.parse
 
       expect(@monthly_reports.length).to eq(1)
@@ -29,11 +29,24 @@ describe IndexReportsSince do
       non_matching_date = '06/21/2017'
       index_reports = IndexReportsSince.new(ARBITRARY_URL, non_matching_date)
       expect(index_reports).to receive(:open).with(ARBITRARY_URL).and_return(@index_reports_html)
+
       @monthly_reports = index_reports.parse
 
       expect(@monthly_reports.length).to eq(2)
       expect(@monthly_reports[0].url).to eq('http://nuforc.org/webreports/ndxp170707.html')
       expect(@monthly_reports[1].url).to eq('http://nuforc.org/webreports/ndxp170622.html')
+    end
+  end
+
+  describe 'given a date past the last posted date' do
+    it 'returns an empty list' do
+      non_matching_date = '07/08/2017'
+      index_reports = IndexReportsSince.new(ARBITRARY_URL, non_matching_date)
+      expect(index_reports).to receive(:open).with(ARBITRARY_URL).and_return(@index_reports_html)
+
+      @monthly_reports = index_reports.parse
+
+      expect(@monthly_reports.length).to eq(0)
     end
   end
 end
