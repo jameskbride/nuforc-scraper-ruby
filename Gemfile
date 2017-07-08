6046e38da28005ce6e9eec0d9b8d67bee7b3e3aa
Jim Kirkbride <james.kirkbride@gmail.com>
2017-07-08 15:53:05 -0400

    Added command line options.
    Can now scrape for everything or all reports since a given date.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem "nokogiri", "~> 1.6.8"
 gem "httparty", "~> 0.14.0"
 gem "sanitize", "~> 4.4.0"
 gem 'chronic', '~> 0.10.2'
+gem 'commander', '~> 4.4'
 
 group :test do
     gem "rspec", "~> 3.5.0"
--- lib/nuforc_scraper.rb
+++ lib/nuforc_scraper.rb
@@ -1,23 +1,54 @@
 require_relative 'index_reports_by_month'
+require_relative 'index_reports_since'
 require 'securerandom'
+require 'commander'
 
-index_reports_by_month = IndexReportsByMonth.new('http://nuforc.org/webreports/ndxevent.html')
+class NuforcScraper
+  include Commander::Methods
 
-begin
-  monthly_reports = index_reports_by_month.parse
-  monthly_reports.each {|report|
-    incidents = report.parse
-    incidents.each {|incident|
-      begin
-        incident.parse
-        incident_json = incident.to_json
-        File.write("#{ARGV[0]}/#{SecureRandom.uuid}.json", incident_json)
-      rescue
-        puts "Couldn't parse the incident"
+  def run
+    program :name, 'Nuforc Scraper'
+    program :version, '1.0.0'
+    program :description, 'Scrapes the NUFORC UFO Database found at: http://nuforc.org'
+
+    command :scrape do |c|
+      c.syntax = 'scrape DIR'
+      c.description = 'Scrapes all reports and writes them to the provided directory DIR'
+      c.option '--since mm/dd/YYYY', String, 'Scrapes reports posted since a given date'
+      c.action do |args, options|
+        if options.since
+          reporter = IndexReportsSince.new('http://nuforc.org/webreports/ndxpost.html', options.since)
+        else
+          reporter = IndexReportsByMonth.new('http://nuforc.org/webreports/ndxevent.html')
+        end
+
+        index_reports(reporter, args.first)
       end
-    }
+    end
 
-  }
-rescue
-  puts "Couldn't parse the index"
+    run!
+  end
+
+  def index_reports(reporter, dir)
+    begin
+      monthly_reports = reporter.parse
+      monthly_reports.each {|report|
+        incidents = report.parse
+        incidents.each {|incident|
+          begin
+            incident.parse
+            incident_json = incident.to_json
+            File.write("#{dir}/#{SecureRandom.uuid}.json", incident_json)
+          rescue
+            puts "Couldn't parse the incident"
+          end
+        }
+
+      }
+    rescue
+      puts "Couldn't parse the index"
+    end
+  end
 end
+
+NuforcScraper.new.run if $0 == __FILE__
